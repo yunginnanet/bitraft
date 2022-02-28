f566a661fbd67d446dbd48ac4c742d4b2e348f44
kayos@tcp.direct <kayos@tcp.direct>
2022-02-28 02:32:07 -0800

    Documentation + refactor positioning

---
+++ commands.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"path/filepath"
+
+	"git.tcp.direct/Mirrors/bitcask-mirror"
+	"github.com/tidwall/finn"
+	"github.com/tidwall/redcon"
+)
+
+func (kvm *Machine) cmdSet(
+	m finn.Applier, conn redcon.Conn, cmd redcon.Command,
+) (interface{}, error) {
+	if len(cmd.Args) != 3 {
+		return nil, finn.ErrWrongNumberOfArguments
+	}
+	return m.Apply(conn, cmd,
+		func() (interface{}, error) {
+			kvm.mu.Lock()
+			defer kvm.mu.Unlock()
+			return nil, kvm.db.Put(cmd.Args[1], cmd.Args[2])
+		},
+		func(v interface{}) (interface{}, error) {
+			conn.WriteString("OK")
+			return nil, nil
+		},
+	)
+}
+
+func (kvm *Machine) cmdEcho(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
+	if len(cmd.Args) != 2 {
+		return nil, finn.ErrWrongNumberOfArguments
+	}
+	conn.WriteBulk(cmd.Args[1])
+	return nil, nil
+}
+
+func (kvm *Machine) cmdGet(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
+	if len(cmd.Args) != 2 {
+		return nil, finn.ErrWrongNumberOfArguments
+	}
+	key := cmd.Args[1]
+	return m.Apply(conn, cmd, nil,
+		func(interface{}) (interface{}, error) {
+			kvm.mu.RLock()
+			defer kvm.mu.RUnlock()
+			value, err := kvm.db.Get(key)
+			if err != nil {
+				if err == bitcask.ErrKeyNotFound {
+					conn.WriteNull()
+					return nil, nil
+				}
+				return nil, err
+			}
+			conn.WriteBulk(value)
+			return nil, nil
+		},
+	)
+}
+
+func (kvm *Machine) cmdDel(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
+	var startIdx = 1
+	return m.Apply(conn, cmd,
+		func() (interface{}, error) {
+			kvm.mu.Lock()
+			defer kvm.mu.Unlock()
+			var n int
+			for i := startIdx; i < len(cmd.Args); i++ {
+				key := cmd.Args[i]
+				err := kvm.db.Delete(key)
+				if err != nil {
+					return 0, err
+				}
+				n++
+			}
+			return n, nil
+		},
+		func(v interface{}) (interface{}, error) {
+			n := v.(int)
+			conn.WriteInt(n)
+			return nil, nil
+		},
+	)
+}
+
+func (kvm *Machine) cmdKeys(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
+	if len(cmd.Args) != 2 {
+		return nil, errWrongNumberOfArguments
+	}
+	pattern := string(cmd.Args[1])
+	scanPattern := trimPattern(pattern)
+	return m.Apply(conn, cmd, nil,
+		func(interface{}) (interface{}, error) {
+			kvm.mu.RLock()
+			defer kvm.mu.RUnlock()
+			var keys [][]byte
+			err := kvm.db.Scan([]byte(scanPattern), func(key []byte) error {
+				if ok, _ := filepath.Match(pattern, string(key)); ok {
+					keys = append(keys, []byte(key))
+				}
+				return nil
+			})
+			if err != nil {
+				return nil, err
+			}
+			conn.WriteArray(len(keys))
+			for i := 0; i < len(keys); i++ {
+				conn.WriteBulk(keys[i])
+			}
+			return nil, nil
+		},
+	)
+}
+
+func (kvm *Machine) cmdFlushdb(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
+	if len(cmd.Args) != 1 {
+		return nil, finn.ErrWrongNumberOfArguments
+	}
+	return m.Apply(conn, cmd,
+		func() (interface{}, error) {
+			kvm.mu.Lock()
+			defer kvm.mu.Unlock()
+			if err := kvm.db.Sync(); err != nil {
+				panic(err.Error())
+			}
+			return nil, nil
+		},
+		func(v interface{}) (interface{}, error) {
+			conn.WriteString("OK")
+			return nil, nil
+		},
+	)
+}
--- server.go
+++ server.go
@@ -100,7 +100,7 @@ func AcceptConnection(conn redcon.Conn) bool {
 
 type cmdHandler func(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error)
 
-// Machine is the FSM for the Raft consensus
+// Machine is the FSM for the Raft consensus.
 type Machine struct {
 	mu     sync.RWMutex
 	dir    string
@@ -112,6 +112,7 @@ type Machine struct {
 	cmdMapper map[string]cmdHandler
 }
 
+// NewMachine constructs ta Machine type for our finite state machine for Raft consensus that will power our database.
 func NewMachine(dir string) (*Machine, error) {
 	kvm := &Machine{
 		dir: dir,
@@ -140,6 +141,7 @@ func (kvm *Machine) Close() (err error) {
 	return
 }
 
+// Command is a callback for incoming Redis commands.
 func (kvm *Machine) Command(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
 	slog := log.With().Str("caller", conn.RemoteAddr()).Str("received", string(cmd.Args[0])).Logger()
 	slog.Trace().Msg(string(cmd.Raw))
@@ -307,128 +309,4 @@ func (kvm *Machine) Snapshot(wr io.Writer) error {
 	}
 
 	return gzw.Close()
-}
-
-func (kvm *Machine) cmdSet(
-	m finn.Applier, conn redcon.Conn, cmd redcon.Command,
-) (interface{}, error) {
-	if len(cmd.Args) != 3 {
-		return nil, finn.ErrWrongNumberOfArguments
-	}
-	return m.Apply(conn, cmd,
-		func() (interface{}, error) {
-			kvm.mu.Lock()
-			defer kvm.mu.Unlock()
-			return nil, kvm.db.Put(cmd.Args[1], cmd.Args[2])
-		},
-		func(v interface{}) (interface{}, error) {
-			conn.WriteString("OK")
-			return nil, nil
-		},
-	)
-}
-
-func (kvm *Machine) cmdEcho(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
-	if len(cmd.Args) != 2 {
-		return nil, finn.ErrWrongNumberOfArguments
-	}
-	conn.WriteBulk(cmd.Args[1])
-	return nil, nil
-}
-
-func (kvm *Machine) cmdGet(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
-	if len(cmd.Args) != 2 {
-		return nil, finn.ErrWrongNumberOfArguments
-	}
-	key := cmd.Args[1]
-	return m.Apply(conn, cmd, nil,
-		func(interface{}) (interface{}, error) {
-			kvm.mu.RLock()
-			defer kvm.mu.RUnlock()
-			value, err := kvm.db.Get(key)
-			if err != nil {
-				if err == bitcask.ErrKeyNotFound {
-					conn.WriteNull()
-					return nil, nil
-				}
-				return nil, err
-			}
-			conn.WriteBulk(value)
-			return nil, nil
-		},
-	)
-}
-
-func (kvm *Machine) cmdDel(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
-	var startIdx = 1
-	return m.Apply(conn, cmd,
-		func() (interface{}, error) {
-			kvm.mu.Lock()
-			defer kvm.mu.Unlock()
-			var n int
-			for i := startIdx; i < len(cmd.Args); i++ {
-				key := cmd.Args[i]
-				err := kvm.db.Delete(key)
-				if err != nil {
-					return 0, err
-				}
-				n++
-			}
-			return n, nil
-		},
-		func(v interface{}) (interface{}, error) {
-			n := v.(int)
-			conn.WriteInt(n)
-			return nil, nil
-		},
-	)
-}
-
-func (kvm *Machine) cmdKeys(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
-	if len(cmd.Args) != 2 {
-		return nil, errWrongNumberOfArguments
-	}
-	pattern := string(cmd.Args[1])
-	scanPattern := trimPattern(pattern)
-	return m.Apply(conn, cmd, nil,
-		func(interface{}) (interface{}, error) {
-			kvm.mu.RLock()
-			defer kvm.mu.RUnlock()
-			var keys [][]byte
-			err := kvm.db.Scan([]byte(scanPattern), func(key []byte) error {
-				if ok, _ := filepath.Match(pattern, string(key)); ok {
-					keys = append(keys, []byte(key))
-				}
-				return nil
-			})
-			if err != nil {
-				return nil, err
-			}
-			conn.WriteArray(len(keys))
-			for i := 0; i < len(keys); i++ {
-				conn.WriteBulk(keys[i])
-			}
-			return nil, nil
-		},
-	)
-}
-
-func (kvm *Machine) cmdFlushdb(m finn.Applier, conn redcon.Conn, cmd redcon.Command) (interface{}, error) {
-	if len(cmd.Args) != 1 {
-		return nil, finn.ErrWrongNumberOfArguments
-	}
-	return m.Apply(conn, cmd,
-		func() (interface{}, error) {
-			kvm.mu.Lock()
-			defer kvm.mu.Unlock()
-			if err := kvm.db.Sync(); err != nil {
-				panic(err.Error())
-			}
-			return nil, nil
-		},
-		func(v interface{}) (interface{}, error) {
-			conn.WriteString("OK")
-			return nil, nil
-		},
-	)
 }
